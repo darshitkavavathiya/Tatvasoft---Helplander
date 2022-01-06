18ee5e26aa74f7ab085e59f0e21e72ed5ecb49a7
darshitbkavathiya <darshitbkavathiya@gmail.com>
2022-01-06 19:40:50 +0530

    admin page psd-html

---
+++ HTML/js/admin.js
@@ -0,0 +1,15 @@
+
+        function reply_click(clicked_id) {
+            var id = clicked_id + "_popup"
+            var obj = document.getElementById(id);
+
+            obj.classList.toggle("show");
+        }
+
+        function reply(clicked_id) {
+            var id = clicked_id + "_popup"
+            var obj = document.getElementById(id);
+
+            obj.classList.toggle("show");
+        }
+    
