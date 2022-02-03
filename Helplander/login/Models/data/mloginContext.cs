4fee9ccfe6a919123eb1afbbf87e61a6e5dce857
darshitkavavathiya <darshitbkavathiya@gmail.com>
2022-02-03 12:24:57 +0530

    Change in Layout View and update static page

---
+++ Helplander/login/Models/data/mloginContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using login.Models;
+
+#nullable disable
+
+namespace login.Models.data
+{
+    public partial class mloginContext : DbContext
+    {
+        public mloginContext()
+        {
+        }
+
+        public mloginContext(DbContextOptions<mloginContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tblogin> Tblogins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ITACHI; Database=mlogin;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Tblogin>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("tblogin");
+
+                entity.Property(e => e.CPassword)
+                    .HasMaxLength(10)
+                    .HasColumnName("c password")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(10)
+                    .HasColumnName("name")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(10)
+                    .HasColumnName("password")
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
