bbd8aa419149dbd27e7bbca589a82b8e0f9e16c5
darshitkavavathiya <darshitbkavathiya@gmail.com>
2022-02-17 22:33:37 +0530

    invoice

--- Helperland/Helperland/wwwroot/js/bookService.js
+++ Helperland/Helperland/wwwroot/js/bookService.js
@@ -1,11 +1,12 @@
+
 function set_service(evt, level) {
 
     var ch = parseInt(level.charAt(2));
-    
-    for (j = ch+1; j <= 4; j++) {
-      
+
+    for (j = ch + 1; j <= 4; j++) {
+
         document.getElementById("form" + j + "Btn").disabled = true;
-        
+
     }
 
 
@@ -15,18 +16,20 @@ function set_service(evt, level) {
         tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
-    //for (i = 0; i < tablinks.length; i++) {
-    //    tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //}
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
     document.getElementById(level).style.display = "block";
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
 
 
+
 function extrabtnclick(id, i) {
 
-    var discount = parseInt(document.getElementById("discount").innerHTML)
+    var discount = parseInt(document.getElementById("discount").innerHTML);
+    var promo = parseInt(document.getElementById("promo").innerHTML);
     if (
 
         document.getElementById(id).checked) {
@@ -35,11 +38,10 @@ function extrabtnclick(id, i) {
         var total_time = parseFloat(document.getElementById("total_hours").innerHTML);
         document.getElementById("total_hours").innerHTML = total_time + 0.5;
         var total_price = parseFloat(document.getElementById("total_price").innerHTML);
-        document.getElementById("total_price").innerHTML = total_price + 5;
-        document.getElementById("priceAfterDiscount").innerHTML = total_price + 5 - discount;
-        document.getElementById("priceAfterPromo").innerHTML = total_price + 5 - discount -10;
-        
-
+        document.getElementById("total_price").innerHTML = total_price + 12.5;
+        document.getElementById("priceAfterDiscount").innerHTML = total_price + 12.5 - discount;
+        document.getElementById("priceAfterPromo").innerHTML = total_price + 12.5 - discount - promo;
+      
     } else {
 
         document.getElementById(id + "Img").src = (" ../images/" + i + ".png");
@@ -47,9 +49,89 @@ function extrabtnclick(id, i) {
         var total_time = parseFloat(document.getElementById("total_hours").innerHTML);
         document.getElementById("total_hours").innerHTML = total_time - 0.5;
         var total_price = parseFloat(document.getElementById("total_price").innerHTML);
-        document.getElementById("total_price").innerHTML = total_price - 5;
-        document.getElementById("priceAfterDiscount").innerHTML = total_price - 5 - discount;
-        document.getElementById("priceAfterPromo").innerHTML = total_price - 5 - discount - 10;
+        document.getElementById("total_price").innerHTML = total_price - 12.5;
+        document.getElementById("priceAfterDiscount").innerHTML = total_price - 12.5 - discount;
+        document.getElementById("priceAfterPromo").innerHTML = total_price - 12.5 - discount - promo;
 
     }
 }
+
+
+
+
+
+$(document).ready(function () {
+
+
+    var ServiceTime = $(".ForTime option:selected").text();
+    document.querySelector(".SIForTime").innerHTML = ServiceTime;
+
+    $(".ForTime").on("change", function () {
+        var ChangeServiceTime = $(".ForTime option:selected").text();
+        document.querySelector(".SIForTime").innerHTML = ChangeServiceTime;
+
+    });
+
+});
+
+
+
+
+$(document).ready(function () {
+
+
+    var Duration = $(".ForDuration option:selected").val();
+    document.querySelector(".SIForDuration").innerHTML = Duration;
+
+    $(".ForDuration").on("change", function () {
+        var basic_time = $(".SIForDuration").text();
+        var total_time = $("#total_hours").text();
+        var extra_time = parseFloat(total_time - basic_time);
+        var ChangeDuration = $(".ForDuration option:selected").val();
+        var newtotal_time = parseFloat(ChangeDuration) + extra_time;
+        var discount = parseInt(document.querySelector("#discount").innerHTML);
+        var promo = parseInt(document.querySelector("#promo").innerHTML);
+        var new_price = newtotal_time * 25;
+        document.querySelector(".SIForDuration").innerHTML = ChangeDuration;
+        document.querySelector("#total_hours").innerHTML = newtotal_time;
+        document.querySelector("#total_price").innerHTML = new_price;
+        document.querySelector('#priceAfterDiscount').innerHTML = new_price - discount;
+        document.querySelector('#priceAfterPromo').innerHTML = new_price - discount - promo;
+
+    });
+
+});
+
+
+
+
+
+
+
+$(document).ready(function () {
+    //var currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    //var day = currentDate.getDate();
+    //var month = currentDate.getMonth() + 1;
+    //var year = currentDate.getFullYear();
+    //var tomorrow_date = day + "/" + month + "/" + year;
+    //document.querySelector('.SIForDate').innerHTML = tomorrow_date;
+
+    //$(".ForDate").datepicker({
+    //    dateFormat: "dd/mm/yy",
+    //    minDate: "+1d",
+
+    //});
+
+
+
+    $(".ForDate").on("change", function () {
+        selected = $(this).val();
+
+        document.querySelector('.SIForDate').innerHTML = selected;
+       
+    });
+});
+
+
+
+
